61e6516f5e8a9bc13d5b27d380fbe37f30c50ea8
Tanuka Das <das.tanuka@yahoo.com>
2019-03-28 05:55:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,5 @@ function updateObjectWithKeyAndValue(object, key, value){
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   //updates `object` with the given `key` and `value` (it is destructive) and returns the entire updated object.
   object[key] = value
-
   return object
 }
